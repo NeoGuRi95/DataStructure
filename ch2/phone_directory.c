978128723d7d922f06ee7f613c214f26714bd560
Jeahyun Park <jea5158@gmail.com>
2022-04-16 13:31:07 +0900

    phonebook01

--- ch2/phone_directory.c
+++ ch2/phone_directory.c
@@ -10,24 +10,25 @@ int n = 0;
 
 void add();
 void find();
-
+void status();
+void remove();
 
 int main()
 {
     char command[BUFFER_SIZE];
  
     while (1) {
-        printf("test$ ");
+        printf("$ ");
         scanf("%s", command);
 ;
         if (strcmp(command, "add") == 0)
             add();
         else if (strcmp(command, "find") == 0)
             find();
-        // else if (strcmp(command, "status") == 0)
-        //     status();
-        // else if (strcmp(command, "remove") == 0)
-        //     remove();
+        else if (strcmp(command, "status") == 0)
+            status();
+        else if (strcmp(command, "remove") == 0)
+            remove();
         else if (strcmp(command, "exit") == 0)
             break;
     }
@@ -49,16 +50,39 @@ void add() {
 }
 
 void find() {
-    char name[BUFFER_SIZE];
+    char buf[BUFFER_SIZE];
 
-    scanf("%s", name);
-    printf("test");
+    scanf("%s", buf);
 
     for (int i = 0; i < n; i++){
-        if (strcmp(name, names[i]) == 0) {
-            printf("%s", numbers[i]);
+        if (strcmp(buf, names[i]) == 0) {
+            printf("%s\n", numbers[i]);
             return;
         } 
     }
-    printf("No person named \'%s\' exists", name);
+    printf("No person named '%s' exists.\n", buf);
+}
+
+void status() {
+    for (int i = 0; i < n; i++) {
+        printf("%s ", names[i]);
+        printf("%s\n", numbers[i]);
+    }
+}
+
+void remove() {
+    char buf[BUFFER_SIZE];
+
+    scanf("%s", buf);
+
+    for (int i = 0; i < n; i++) {
+        if (strcmp(buf, names[i]) == 0){
+            names[i] = names[n-1];
+            numbers[i] = numbers[n-1];
+            n--;
+            printf("'%s' was deleted successfully.\n", buf);
+            return;
+        }
+    }
+    printf("No person named '%s' exists.\n", buf);
 }
